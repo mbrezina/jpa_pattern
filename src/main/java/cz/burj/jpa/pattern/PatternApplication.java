9de38fb2cb27ead5725fa1f4273943b59127aab5
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-08-22 22:30:44 +0200

    initial commit

---
+++ src/main/java/cz/burj/jpa/pattern/PatternApplication.java
@@ -0,0 +1,13 @@
+package cz.burj.jpa.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cz/burj/jpa/pattern/UserRepository.java
@@ -0,0 +1,12 @@
+package cz.burj.jpa.pattern;
+
+import org.springframework.data.repository.CrudRepository;
+
+import cz.burj.jpa.pattern.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
