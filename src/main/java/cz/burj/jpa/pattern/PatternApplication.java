489b9f14dd2205c103be86a838f5bcba97500702
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-08-23 08:13:25 +0200

    enable app to open the home page

--- src/main/java/cz/burj/jpa/pattern/PatternApplication.java
+++ src/main/java/cz/burj/jpa/pattern/PatternApplication.java
@@ -1,13 +1,32 @@
 package cz.burj.jpa.pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class PatternApplication {
 
-	public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(PatternApplication.class);
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(PatternApplication.class, args);
 	}
 
+
+    /**
+     * Posluchac udalosti nasazeni webove aplikace na Tomcat, ktery zaloguje adresu,
+     * na ktere je aplikace nasazena
+     * @param evt The event object
+     */
+    @EventListener
+    public void onAppEvent(ServletWebServerInitializedEvent evt) {
+        int port = evt.getApplicationContext().getWebServer().getPort();
+        logger.info("Your web app address: http://localhost:" + port +
+            evt.getApplicationContext().getServletContext().getContextPath());
+    }
 }
